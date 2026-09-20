01116f1911f6d8bdddad36d8acd7ff3a79c1679a
Mohammad Eftekhari <turkaslan1206@gmail.com>
2026-09-20 21:36:30 +0330

    user migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('family');
+            $table->integer('phoneNumber');
+            $table->string('main_image');
+            $table->string('ref_code');
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
