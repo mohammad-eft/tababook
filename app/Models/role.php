4737d5149af613d25c73d3fd2b2096692f1f6610
Mohammad Eftekhari <turkaslan1206@gmail.com>
2026-09-20 21:49:23 +0330

    relations

--- app/Models/role.php
+++ app/Models/role.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class role extends Model
 {
-    //
+    protected $fillable = [
+        'en_title',
+        'fa_title',
+    ];
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
 }
