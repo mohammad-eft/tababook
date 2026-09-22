4ecb10561d8e5c84b87692ffc32c83ea418c94de
hossein <hosseinfaos1386@gmail.com>
2026-09-22 18:34:48 +0330

    insert-newhome

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,11 +1,23 @@
-let hamburgerMenuIcon = document.getElementById("hamburgerMenu");
+let hamburger_menu_item = document.getElementById('hamburger_menu_item')
+        let hamburger_menu_item_close = document.getElementById('hamburger_menu_item_close')
 
-function hamburgerMenu() {
-    hamburgerMenuIcon.classList.remove("-right-full");
-    hamburgerMenuIcon.classList.add("right-0");
-}
+        function hamburger_menu(type) {
+            if (type == 'open') {
+                hamburger_menu_item.classList.remove('translate-x-full')
+                hamburger_menu_item_close.classList.remove('invisible')
+                hamburger_menu_item_close.classList.remove('opacity-0')
+            }
+            if (type == 'close') {
+                hamburger_menu_item.classList.add('translate-x-full')
+                hamburger_menu_item_close.classList.add('invisible')
+                hamburger_menu_item_close.classList.add('opacity-0')
+            }
+        }
 
-function removeHamburgerMenu() {
-    hamburgerMenuIcon.classList.remove("right-0");
-    hamburgerMenuIcon.classList.add("-right-full");
-}
+        let change_like_svh = document.querySelectorAll('.change_like_svh')
+        change_like_svh.forEach((item)=>{
+            item.addEventListener('click' , function(){
+                item.children[0].classList.toggle('hidden')
+                item.children[1].classList.toggle('hidden')
+            })
+        })
