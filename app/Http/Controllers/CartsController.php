875ba430d12e6220d598769d43329e52afeb3e33
Mohammad Eftekhari <turkaslan1206@gmail.com>
2026-09-23 16:24:51 +0330

    cart and order backend

---
+++ app/Http/Controllers/CartsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\carts;
+use App\Models\User;
+use App\Models\product;
+
+class CartsController extends Controller
+{
+    public function store(Request $request)
+    {
+        $user_id = Auth::id();
+        if (!Auth::check()) {
+            $user_id = $request->input('user_id');
+        }
+        $cart = carts::create([
+            'product_id' => $request->product_id,
+            'user_id' => $user_id,
+            'quantity' => $request->quantity ? $request->quantity : 1,
+        ]);
+
+        return response()->json($cart);
+    }
+    public function delete(Request $request)
+    {
+        $cart = carts::where('user_id', $request->user_id)->where('product_id', $request->product_id)->where('order_id', null)->first();
+        $data = $cart;
+        if ($cart) {
+            $cart->delete();
+        }
+
+        $remainingItems = carts::where('user_id', $request->user_id)->where('order_id', null)->get();
+        $count = $remainingItems->sum('quantity');
+
+        return response()->json([
+            'message' => 'محصول از سبد خرید حذف شد',
+            'data' => $data,
+            'count' => $count
+        ]);
+    }
+    function update(Request $request)
+    {
+        $user_id = $request->input('user_id');
+        if (Auth::check()) {
+            $user_id = Auth::id();
+        }
+        $cart = carts::where(['product_id' => $request->product_id, 'user_id' => $user_id, 'order_id' => null])->first();
+        $cart->quantity = $request->quantity ? $request->quantity : 1;
+        $cart->save();
+
+        return response()->json($cart);
+    }
+
+    public function showCarts(Request $request)
+    {
+        $carts = carts::where('user_id', $request->user_id)->where('order_id', null)->get();
+        $total_price = 0;
+        $cartData = [];
+        foreach ($carts as $cart) {
+            $product = $cart->product;
+            $price = $cart->product->secondary_price ? $cart->product->secondary_price : $cart->product->primary_price;
+            $total_price += $price * $cart->quantity;
+
+           
+                if ($product->media->isNotEmpty()) {
+                    foreach ($product->media as $media) {
+                        if ($media['is_main']) {
+                            $product['mainImg']  = $media['media_path'];
+                            break;
+                        } else {
+                            $product['mainImg'] = 'default.jpg';
+                        }
+                    }
+                } else {
+                    $product['mainImg'] = 'default.jpg';
+                }
+            
+
+            $cartData[] = [
+                'summary' => $cart->product->summary,
+                'user_id' => $cart->user_id,
+                'id' => $cart->id,
+                'product_id' => $cart->product_id,
+                'product_name' => $cart->product->title ?? 'محصول',
+                'quantity' => $cart->quantity,
+                'price' => $price,
+                'total' => $price * $cart->quantity,
+                'img' => $product->mainImg
+            ];
+        }
+
+        return response()->json([
+            'success' => true,
+            'carts' => $cartData,
+            'total_price' => $total_price,
+            'count' => $carts->sum('quantity')
+        ]);
+    }
+}
---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\orders;
+use App\Models\carts;
+use App\Models\category;
+use App\Models\defaultComment;
+use App\Models\logo;
+use App\Models\product;
+use App\Models\User;
+use App\Models\header;
+use App\Models\introduction;
+use App\Models\service;
+use Hekmatinasser\Verta\Verta;
+
+class OrdersController extends Controller
+{
+    public function store(Request $request)
+    {
+        $user_id = Auth::id();
+        if (isset($request->user_id)) {
+            $user_id = $request->user_id;
+        }
+
+        // همه آیتم‌های سبد خرید کاربر که هنوز سفارش نشدن
+        $cartItems = carts::where('user_id', $user_id)
+            ->where('order_id', null)
+            ->get();
+
+        if ($cartItems->isEmpty()) {
+            return response()->json(['message' => 'سبد خرید خالی است'], 400);
+        }
+
+        $createdOrders = [];
+        $dateTime = explode(' ', verta());
+        $date = implode('/', explode('-', $dateTime[0]));
+        $time = $dateTime[1];
+        $order_id = orders::insertGetId([
+            'address_id' => isset($request->address) ? $request->address : null,
+            'user_id' => $user_id,
+            'order_status_id' => 1,
+            'date'=>$date,
+            'time'=>$time
+        ]);
+        foreach ($cartItems as $cartItem) {
+
+            $order_code = "100" . $order_id;
+            orders::where('id', $order_id)->update(['order_code' => $order_code]);
+
+            $cartItem->order_id = $order_id;
+            $cartItem->save();
+
+            $createdOrders[] = [
+                'order_id' => $order_id,
+                'product_id' => $cartItem->product_id,
+            ];
+        }
+
+        return response()->json($createdOrders);
+    }
+
+    public function index(){
+        $orders = orders::all();
+        $header = header::first();
+        $logo = logo::first();
+        $services = service::all();
+        $introduction = introduction::first();
+        $categories = category::with('products')->has('products')->get();
+        $products = product::where('show_in_home', 1)->get();
+        $defaultComments = defaultComment::all();
+        foreach ($products as $product) {
+            if ($product->media->isNotEmpty()) {
+                foreach ($product->media as $media) {
+                    if ($media['is_main']) {
+                        $product['mainImg']  = $media['media_path'];
+                        break;
+                    } else {
+                        $product['mainImg'] = 'default.jpg';
+                    }
+                }
+            } else {
+                $product['mainImg'] = 'default.jpg';
+            }
+        }
+        return view('admin.order.index', [
+            'logo' => $logo,
+            'header' => $header,
+            'services' => $services,
+            'introduction' => $introduction,
+            'categories' => $categories,
+            'products' => $products,
+            'defaultComments' => $defaultComments,
+            'orders'=>$orders
+        ]);
+    }
+
+    public function showItems(orders $order){
+        $order->load(['carts'=>function($query){
+            $query->with(['product'=>function($q){
+                $q->with(['media'=>function($qr){
+                    $qr->where('is_main', 1)->get();
+                }]);
+            }]);
+        }]);
+        return response()->json($order);
+    }
+
+    public function cancelAll(User $user){
+        carts::where('user_id', $user->id)->whereNull('order_id')->delete();
+        return response()->json('ok');
+    }
+}
