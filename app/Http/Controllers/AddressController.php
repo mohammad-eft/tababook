c9cf70dd6f8b6c89c8272c25c8a1130feba9bf21
Mohammad Eftekhari <turkaslan1206@gmail.com>
2026-09-23 16:53:29 +0330

    contactus and about us backend

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\address;
+
+class AddressController extends Controller
+{
+    //
+}
--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -64,13 +64,10 @@ class OrdersController extends Controller
 
     public function index(){
         $orders = orders::all();
-        $header = header::first();
-        $logo = logo::first();
-        $services = service::all();
-        $introduction = introduction::first();
+        
         $categories = category::with('products')->has('products')->get();
         $products = product::where('show_in_home', 1)->get();
-        $defaultComments = defaultComment::all();
+   
         foreach ($products as $product) {
             if ($product->media->isNotEmpty()) {
                 foreach ($product->media as $media) {
@@ -86,13 +83,8 @@ class OrdersController extends Controller
             }
         }
         return view('admin.order.index', [
-            'logo' => $logo,
-            'header' => $header,
-            'services' => $services,
-            'introduction' => $introduction,
             'categories' => $categories,
             'products' => $products,
-            'defaultComments' => $defaultComments,
             'orders'=>$orders
         ]);
     }
